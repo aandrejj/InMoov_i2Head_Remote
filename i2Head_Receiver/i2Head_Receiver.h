1b38412a6543ef396da342bbeb665cd2a7fd12dc
Andrej Pauliny <andrej.pauliny@gmail.com>
2024-07-11 17:02:54 +0200

    Implement centerPoint

--- i2Head_Receiver/i2Head_Receiver.h
+++ i2Head_Receiver/i2Head_Receiver.h
@@ -32,6 +32,16 @@ int ch_6 = 0;
 int ch_7 = 0;
 int ch_8 = 0;
 
+bool center_points_initialized;
+int ch_1_center = 0;
+int ch_2_center = 0;
+int ch_3_center = 0;
+int ch_4_center = 0;
+int ch_5_center = 0;
+int ch_6_center = 0;
+int ch_7_center = 0;
+int ch_8_center = 0;
+
 int ch_1_constrained = 0;
 int ch_2_constrained = 0;
 int ch_3_constrained = 0;
@@ -57,3 +67,20 @@ int servo_upperLip_Angle        ;
 int servo_forheadRight_Angle    ;
 int servo_forheadLeft_Angle     ;
 int servo_Jaw_UpDown_Angle      ;
+
+int servo_eyeLeftUD_Pwm       ;
+int servo_eyeLeftLR_Pwm       ;
+int servo_eyeRightUD_Pwm      ;
+int servo_eyeRightLR_Pwm      ; 
+int servo_eyelidLeftUpper_Pwm ;
+int servo_eyelidLeftLower_Pwm ;
+int servo_eyelidRightUpper_Pwm;
+int servo_eyelidRightLower_Pwm;
+int servo_eyebrowRight_Pwm    ;
+int servo_eyebrowLeft_Pwm     ;
+int servo_cheekRight_Pwm      ;
+int servo_cheekLeft_Pwm       ;
+int servo_upperLip_Pwm        ;
+int servo_forheadRight_Pwm    ;
+int servo_forheadLeft_Pwm     ;
+int servo_Jaw_UpDown_Pwm      ;
